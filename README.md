81329cee0ac98c03bf5c43c66fbf4189302089d9
Artium Nihamkin <artium@nihamkin.com>
2019-06-03 13:33:39 +0300

    Started using the mew api. Two calls now: simulation and response generator and tts

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ui-componenet
 
 
+# Special Installation Command
 
+`sudo npm  install -g electron --unsafe-perm=true --allow-root`
 
--- window.js
+++ window.js
@@ -15,26 +15,46 @@ $(() => {
   context = new AudioContext();
 
   document.getElementById('recordBtn').addEventListener('click', function () {
+    
+    // TODO: Clear variables and GUI
     $("#stt-result").text(stt_text)
     $("#intention-result").text(JSON.stringify(command, null, "\t"))
     $("#tts-to-be-synthesized").text(tts_text)
+    /////
 
 
-    generate_response(function(buff) {
-      var source = context.createBufferSource(); // creates a sound source
-      source.buffer = buff;                      // tell the source which sound to play
-      source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-      source.start(0);                           // play the source now
-                                                 // note: on older systems, may have to use deprecated noteOn(time);
+    get_response(command, function(jsonObj) {
+      $("#tts-to-be-synthesized").text(jsonObj.text)
+
+      get_synth_tts(jsonObj.text, function(buff) {
+        var source = context.createBufferSource(); // creates a sound source
+        source.buffer = buff;                      // tell the source which sound to play
+        source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+        source.start(0);                           // play the source now
+                                                   // note: on older systems, may have to use deprecated noteOn(time);
+      })
     })
   });
 })
 
+function get_response(jsonObj, cb)
+{
+  var oReq = new XMLHttpRequest();
+  oReq.open("POST", "http://127.0.0.1:3000/response", true);
+  oReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  oReq.onload = function (oEvent) {
+    cb(JSON.parse(oReq.response))
+  };
 
-function generate_response(cb) {
+  oReq.send(JSON.stringify(jsonObj));
+
+}
+
+
+function get_synth_tts(text, cb) {
 
   var oReq = new XMLHttpRequest();
-  oReq.open("POST", "http://127.0.0.1:3000/set/frequency?freq=399.00", true);
+  oReq.open("POST", `http://127.0.0.1:3000/synth?text=${text}`, true);
   oReq.responseType = "arraybuffer";
 
   oReq.onload = function (oEvent) {
